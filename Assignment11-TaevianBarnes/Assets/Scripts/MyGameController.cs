0b2ea273acbe388303b17f46026dde4b2df70ea4
Taevian Barnes <tbarnes1930@gmail.com>
2019-03-21 18:10:02 -0400

    Added Enemies

---
+++ Assignment11-TaevianBarnes/Assets/Scripts/MyGameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class MyGameController : NetworkBehaviour
+{
+    public GameObject enemyPrefab;
+
+    private float spawnEnemyTime = 0;
+
+    void Update()
+    {
+        if (isServer)
+        {
+            if (Time.fixedTime > spawnEnemyTime)
+            {
+                SpawnEnemy();
+            }
+        }
+    }
+
+    [Server]
+    public void SpawnEnemy()
+    {
+        Vector3 position = new Vector3(Random.Range(-6.75f, 6.75f), Random.Range(1.0f, 8.0f), 4.5f);
+        GameObject enemy = (GameObject)Instantiate(enemyPrefab, position, Quaternion.identity);
+        NetworkServer.Spawn(enemy);
+        spawnEnemyTime = Time.fixedTime + Random.Range(3, 8);
+    }
+
+}
